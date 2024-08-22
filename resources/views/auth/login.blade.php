c9f9c288828e7886f8fefecea2dd94340ade9c12
Alif Essa Nurcahyani <essanurcahyania@gmail.com>
2024-08-22 23:37:56 +0700

    update

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,59 +6,55 @@
         <!-- Image -->
         <img src="{{ asset('assets/image/masjide.png') }}" alt="" class="h-[520px] mx-auto">
     </div>
-
-    <img src="{{ asset('assets/image/bg-login-hijau.png') }}" alt=""
-        class=" absolute right-0 h-screen -z-5 w-[58%] max-md:hidden">
-    <div
-        class="w-1/2 max-md:w-full max-md:bg-[#40534C] my-auto  flex items-center justify-center mx-auto z-10 min-h-screen">
+    
+    <img src="{{asset('assets/image/bg-login-hijau.png')}}" alt="" class=" absolute right-0 h-screen -z-5 w-[58%] max-md:hidden">
+    <div class="w-1/2 max-md:w-full max-md:bg-[#40534C] my-auto  flex items-center justify-center mx-auto z-10 min-h-screen">
         <form method="POST" action="{{ route('login') }}" class="w-80">
             @csrf
-        
-            <div class="mb-4 w-full">
-                <label class="block font-medium text-sm text-[#D6EFD8] md:text-[#40534C] mb-2" for="email">
-                    Email
-                </label>
-                <input
-                    class="border-[#40534C] bg-[#f5f5f5] focus:border-[#40534C] w-full focus:ring-[#40534C] max-md:focus:border-[#80AF81] max-md:focus:ring-[#80AF81] rounded-md shadow-sm"
-                    id="email" type="email" name="email" placeholder="email">
+    
+            <!-- Email Address -->
+            <div>
+                <x-input-label for="email" :value="__('Email')" />
+                <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')"
+                    required autofocus autocomplete="username" />
                 <x-input-error :messages="$errors->get('email')" class="mt-2" />
             </div>
-        
-            <div class="mb-6 w-full">
-                <label class="block font-medium text-sm text-[#D6EFD8] mb-2 md:text-[#40534C]" for="password">
-                    Password
-                </label>
-                <input
-                    class="border-[#40534C] bg-[#f5f5f5] focus:border-[#40534C] w-full focus:ring-[#40534C] max-md:focus:border-[#80AF81] max-md:focus:ring-[#80AF81] rounded-md shadow-sm"
-                    id="password" type="password" name="password" placeholder="password">
+    
+            <!-- Password -->
+            <div class="mt-4">
+                <x-input-label for="password" :value="__('Password')" />
+    
+                <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required
+                    autocomplete="current-password" />
+    
                 <x-input-error :messages="$errors->get('password')" class="mt-2" />
             </div>
-        
+    
+            <!-- Remember Me -->
             <div class="flex justify-between mt-4 mb-4">
                 <label for="remember_me" class="inline-flex items-center">
                     <input id="remember_me" type="checkbox"
                         class="rounded border-gray-300 text-[#80AF81] shadow-sm focus:ring-[#40534C]" name="remember">
                     <span class="ms-2 text-sm text-[#40534C] max-md:text-[#80AF81]">{{ __('Remember me') }}</span>
                 </label>
+                @if (Route::has('password.request'))
+                    <a class="underline text-sm text-start text-[#40534C] max-md:text-[#80AF81] hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                        href="{{ route('password.request') }}">
+                        {{ __('Forgot your password?') }}
+                    </a>
+                @endif
             </div>
-        
-            <div class="flex items-center justify-between">
-                <button
-                    class="inline-flex items-center justify-center w-full px-4 py-4 bg-[#40534C] border border-transparent rounded-lg font-semibold text-xs text-[#D6EFD8] uppercase tracking-widest hover:bg-[#D6EFD8] hover:text-[#40534C] hover:border-[#40534C] max-md:bg-[#D6EFD8] max-md:text-[#40534C] max-md:hover:bg-[#40534C] max-md:hover:text-[#D6EFD8] max-md:hover:border-[#D6EFD8] focus:outline-none focus:ring-2 focus:ring-[#40534C] max-md:focus:ring-[#D6EFD8] transition ease-in-out duration-150"
-                    type="submit">
-                    Log In
-                </button>
-            </div>
-        
-            <div class="max-w-full text-center mt-4">
-                <p class="text-[#40534C] max-md:text-[#80AF81]">Don't have an account? 
-                    <span class="text-[#D6EFD8]">
-                        <a href="{{ route('register') }}">Sign up here</a>
-                    </span>
-                </p>
+    
+            <div class="max-w-full text-center">
+    
+                <x-primary-button class="max-w-full w-full rounded-lg mb-3">
+                    {{ __('Log in') }}
+                </x-primary-button>
+                <p class="text-[#40534C] max-md:text-[#80AF81]">Don't have an account ? <span class="text-[#D6EFD8]"><a href="{{ route('register') }}">Sign up
+                            here</a>
+                    </span></p>
             </div>
         </form>
-        
     </div>
-
+    
 </x-guest-layout>
