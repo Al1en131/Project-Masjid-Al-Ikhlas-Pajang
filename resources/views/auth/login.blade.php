95eba31c31c465f4dabc50e87057a5bc9e20d328
Alif Essa Nurcahyani <essanurcahyania@gmail.com>
2024-08-22 23:46:41 +0700

    update

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,12 +6,14 @@
         <!-- Image -->
         <img src="{{ asset('assets/image/masjide.png') }}" alt="" class="h-[520px] mx-auto">
     </div>
-    
-    <img src="{{asset('assets/image/bg-login-hijau.png')}}" alt="" class=" absolute right-0 h-screen -z-5 w-[58%] max-md:hidden">
-    <div class="w-1/2 max-md:w-full max-md:bg-[#40534C] my-auto  flex items-center justify-center mx-auto z-10 min-h-screen">
+
+    <img src="{{ asset('assets/image/bg-login-hijau.png') }}" alt=""
+        class=" absolute right-0 h-screen -z-5 w-[58%] max-md:hidden">
+    <div
+        class="w-1/2 max-md:w-full max-md:bg-[#40534C] my-auto  flex items-center justify-center mx-auto z-10 min-h-screen">
         <form method="POST" action="{{ route('login') }}" class="w-80">
             @csrf
-    
+
             <!-- Email Address -->
             <div>
                 <x-input-label for="email" :value="__('Email')" />
@@ -19,17 +21,17 @@
                     required autofocus autocomplete="username" />
                 <x-input-error :messages="$errors->get('email')" class="mt-2" />
             </div>
-    
+
             <!-- Password -->
             <div class="mt-4">
                 <x-input-label for="password" :value="__('Password')" />
-    
+
                 <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required
                     autocomplete="current-password" />
-    
+
                 <x-input-error :messages="$errors->get('password')" class="mt-2" />
             </div>
-    
+
             <!-- Remember Me -->
             <div class="flex justify-between mt-4 mb-4">
                 <label for="remember_me" class="inline-flex items-center">
@@ -44,17 +46,18 @@
                     </a>
                 @endif
             </div>
-    
+
             <div class="max-w-full text-center">
-    
+
                 <x-primary-button class="max-w-full w-full rounded-lg mb-3">
                     {{ __('Log in') }}
                 </x-primary-button>
-                <p class="text-[#40534C] max-md:text-[#80AF81]">Don't have an account ? <span class="text-[#D6EFD8]"><a href="{{ route('register') }}">Sign up
+                <p class="text-[#40534C] max-md:text-[#80AF81]">Don't have an account ? <span class="text-[#D6EFD8]"><a
+                            href="{{ route('register') }}">Sign up
                             here</a>
                     </span></p>
             </div>
         </form>
     </div>
-    
+
 </x-guest-layout>
